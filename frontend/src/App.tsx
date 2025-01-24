49d4d1100289d4f84681a81c7ac8197a4f37c5bc
mgallegoa <manuelgallego2003@yahoo.com>
2025-01-23 19:17:56 -0500

    CSV MonoRepo : Logic for upload the csv file.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,34 +1,53 @@
 import { useState } from "react";
-
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(6);
+const APP_STATUS = {
+  IDLE: "idle", // start
+  ERROR: "error", // in case of error
+  READY_UPLOAD: "ready_upload",
+  UPLOADING: "uploading",
+  READY_USAGE: "ready_usage",
+} as const;
 
+type AppStatusType = (typeof APP_STATUS)[keyof typeof APP_STATUS];
+
+const BUTTON_TEXT = {
+  [APP_STATUS.READY_UPLOAD]: "Upload File",
+  [APP_STATUS.UPLOADING]: "Uploading...",
+};
+
+function App() {
+  const [appStatus, setAppStatus] = useState<AppStatusType>(APP_STATUS.IDLE);
+  const [file, setFile] = useState<File | null>(null);
+  const handleInputCSV = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const [file] = event.target.files ?? [];
+    if (file) {
+      setFile(file);
+      setAppStatus(APP_STATUS.READY_UPLOAD);
+    }
+  };
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+  };
+
+  const showUploadButton = APP_STATUS.READY_UPLOAD || APP_STATUS.UPLOADING;
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>CSV challenge</h1>
+      <form onSubmit={handleSubmit}>
+        <label>
+          <input
+            onChange={handleInputCSV}
+            type="file"
+            name="file"
+            accept=".csv"
+            disabled={appStatus === APP_STATUS.UPLOADING}
+          />
+        </label>
+        {showUploadButton && (
+          <button type="button">{BUTTON_TEXT[appStatus]}</button>
+        )}
+      </form>
     </>
   );
 }
